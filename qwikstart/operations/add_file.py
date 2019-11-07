a1a6d68a06d5d5848aa0fb6e426a457dcec1b69b
Tony S Yu <tsyu80@gmail.com>
2019-11-06 23:07:16 -0600

    Add `add_file` operation

---
+++ qwikstart/operations/add_file.py
@@ -0,0 +1,42 @@
+import os
+from pathlib import Path
+from typing import Any, Dict, List, Type
+
+import jinja2
+from typing_extensions import TypedDict
+
+from .base import BaseOperation
+
+
+__all__ = ["Operation"]
+
+
+class RequiredContext(TypedDict):
+    target_path: Path
+    template_name: str
+
+
+class Context(RequiredContext, total=False):
+    template_loader: Type[jinja2.BaseLoader]
+    template_loader_args: List[Any]
+    template_loader_kwargs: Dict[str, Any]
+    template_variables: Dict[str, Any]
+
+
+class Operation(BaseOperation):
+    """Operation write file"""
+
+    name: str = "write_file"
+
+    def run(self, context: Context) -> None:
+        loader_class = context.get("template_loader", jinja2.FileSystemLoader)
+        loader_args = context.get("template_loader_args", ())
+        loader_kwargs = context.get("template_loader_kwargs", {})
+        env = jinja2.Environment(
+            loader=loader_class(*loader_args, **loader_kwargs)
+        )
+        template = env.get_template(context["template_name"])
+
+        template_variables = context.get("template_variables", {})
+        with context["target_path"].open("w") as f:
+            f.write(template.render(**template_variables))
---
+++ tests/operations/test_add_file.py
@@ -0,0 +1,39 @@
+from typing import Any, Dict, Optional
+import jinja2
+
+from qwikstart.operations import add_file
+from ..helpers import create_mock_file_path
+
+
+class TestAddFile:
+    def test_file_with_no_variables(self):
+        rendered_string = render_template("Fake template content")
+        assert rendered_string == "Fake template content"
+
+    def test_file_with_variables(self):
+        rendered_string = render_template(
+            """Hello, {{name}}!""", template_variables={"name": "World"}
+        )
+        assert rendered_string == "Hello, World!"
+
+
+def render_template(
+    template_string: str, template_variables: Optional[Dict[str, Any]] = None
+):
+    """Return rendered string given a template and optional variables."""
+    template_variables = template_variables or {}
+
+    output_file = create_mock_file_path("")
+    template_name = "test.txt"
+    context: add_file.Context = {
+        "target_path": output_file,
+        "template_name": template_name,
+        "template_loader": jinja2.DictLoader,
+        "template_loader_args": [{template_name: template_string}],
+        "template_variables": template_variables,
+    }
+    add_file_op = add_file.Operation()
+    add_file_op.execute(context)
+
+    with output_file.open() as f:
+        return f.read()
