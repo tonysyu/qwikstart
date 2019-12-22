1d6eecfba3346ee81d388ed4e2f2057159bdc44d
Tony S Yu <tsyu80@gmail.com>
2019-12-22 10:55:02 -0600

    Add add_file_tree operation

---
+++ qwikstart/operations/add_file_tree.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from typing import Any, Dict, Union
+
+import jinja2
+
+from ..base_context import BaseContext
+from ..utils import ensure_path
+from ..utils.filesystem import FileTreeGenerator
+from ..utils.templates import TemplateRenderer
+from .base import BaseOperation
+
+__all__ = ["Operation"]
+
+
+class RequiredContext(BaseContext):
+    template_dir: str
+
+
+class Context(RequiredContext, total=False):
+    target_dir: Union[Path, str]
+    template_variables: Dict[str, Any]
+    template_variable_prefix: str
+
+
+class Operation(BaseOperation):
+    """Operation to add a file tree (a.k.a. directory) to a project."""
+
+    name: str = "add_file_tree"
+
+    def run(self, context: Context) -> None:
+        execution_context = context["execution_context"]
+        renderer = TemplateRenderer.from_context(context)
+
+        source = execution_context.source_dir.joinpath(context["template_dir"])
+        target = context.get("target_dir", execution_context.target_dir)
+
+        generator = FileTreeGenerator(Path(source), Path(target), renderer)
+        generator.copy()
---
+++ tests/operations/test_add_file_tree.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from unittest.mock import ANY, patch
+
+from qwikstart.operations import add_file_tree
+
+from .. import helpers
+
+
+class TestAddFileTree:
+    def test_generator_initialized_and_called(self):
+        add_file_tree_op = add_file_tree.Operation()
+        template_dir = Path("/path/to/template/dir")
+        target_dir = Path("/path/to/target/dir")
+
+        context: add_file_tree.Context = {
+            "execution_context": helpers.get_execution_context(),
+            "template_dir": template_dir,
+            "target_dir": target_dir,
+        }
+
+        with patch.object(add_file_tree, "FileTreeGenerator") as mock_gen:
+            add_file_tree_op.execute(context)
+
+        # FileTreeGenerator should be initialized:
+        mock_gen.assert_called_once_with(template_dir, target_dir, ANY)
+        # The FileTreeGenerator instance's copy method should be called:
+        mock_gen.return_value.copy.assert_called_once()
