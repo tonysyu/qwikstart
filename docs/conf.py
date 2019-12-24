412bfca0464401c35ae9594426c9c9ec718c7bad
Tony S Yu <tsyu80@gmail.com>
2019-12-24 13:08:16 -0600

    Switch Sphinx theme to alabaster

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ default_role = "literal"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 html_theme_path = ["_themes"]
 
 # Theme options are theme-specific and customize the look and feel of a theme
