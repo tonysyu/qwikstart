8b5eb75323dcbb9ccc61f6afaff7f6a84b1110da
Tony S Yu <tsyu80@gmail.com>
2019-12-25 16:30:25 -0600

    Add test for help cli command

--- tests/cli/test_main.py
+++ tests/cli/test_main.py
@@ -1,6 +1,7 @@
 from click.testing import CliRunner
 
 from qwikstart.cli import main
+from qwikstart.operations import add_file
 from qwikstart.parser import get_operations_mapping
 
 
@@ -11,3 +12,9 @@ def test_list_operations():
 
     for op_name in get_operations_mapping():
         assert op_name in result.output
+
+
+def test_help():
+    runner = CliRunner()
+    result = runner.invoke(main.help, [add_file.Operation.name])
+    assert result.exit_code == 0
