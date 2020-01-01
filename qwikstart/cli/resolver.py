12bb251f6c08359fd382526b41d18892622f7272
Tony S Yu <tsyu80@gmail.com>
2020-01-01 11:27:38 -0600

    Add support for task directories

--- qwikstart/cli/resolver.py
+++ qwikstart/cli/resolver.py
@@ -1,17 +1,19 @@
-#!/usr/bin/env python3
 from pathlib import Path
 from typing import Any, Dict, Optional, cast
 
 import yaml
 
+from ..exceptions import TaskLoaderError
 from ..parser import TaskDefinition, parse_task
 from ..tasks import Task
+
+QWIKSTART_TASK_DEFINITION_FILE = "qwikstart.yml"
 
 
 class YamlLoader:
     known_extensions = {".yaml", ".yml"}
 
-    def can_handle(self, file_path: Path) -> bool:
+    def can_load(self, file_path: Path) -> bool:
         return file_path.suffix in self.known_extensions
 
     def load(self, file_path: Path) -> Dict[str, Any]:
@@ -19,26 +21,27 @@ class YamlLoader:
             return yaml.safe_load(f)
 
 
-loaders_list = [YamlLoader()]
+class LocalPathResolver:
 
+    loader = YamlLoader()
 
-class ResolveLocalPath:
     def __init__(self, path: str, root: Optional[Path] = None):
         root = root or Path(".")
         self.resolved_path = root.joinpath(path).resolve()
+        if self.resolved_path.is_dir():
+            self.resolved_path = self.resolved_path / QWIKSTART_TASK_DEFINITION_FILE
 
     def exists(self) -> bool:
         return self.resolved_path.is_file()
 
     def parsed_data(self) -> Dict[str, Any]:
-        for loader in loaders_list:
-            if loader.can_handle(self.resolved_path):
-                return loader.load(self.resolved_path)
+        if self.loader.can_load(self.resolved_path):
+            return self.loader.load(self.resolved_path)
         else:
-            raise RuntimeError(f"No loader to handle {self.resolved_path!r}")
+            raise TaskLoaderError(f"Cannot load {self.resolved_path!r}")
 
 
-task_resolver_list = [ResolveLocalPath]
+task_resolver_list = [LocalPathResolver]
 
 
 def resolve_task(task_path: str) -> Task:
@@ -54,4 +57,4 @@ def resolve_task(task_path: str) -> Task:
             attempted_paths.append(resolver.resolved_path)
     else:
         attempts = "\n- ".join(str(path) for path in attempted_paths)
-        raise RuntimeError(f"Could not resolve path. Attempted: {attempts}")
+        raise TaskLoaderError(f"Could not resolve path. Attempted: {attempts}")
