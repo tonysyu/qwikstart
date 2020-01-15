1970181877fdd6032eff214ccca0011cc778e36f
Tony S Yu <tsyu80@gmail.com>
2020-01-14 19:39:45 -0600

    Factor out new qwikstart.utils.dict_utils

--- qwikstart/utils/__init__.py
+++ qwikstart/utils/__init__.py
@@ -1,11 +1,5 @@
-from .core import (
-    ensure_path,
-    first,
-    full_class_name,
-    indent,
-    merge_nested_dicts,
-    remap_dict,
-)
+from .core import ensure_path, first, full_class_name, indent
+from .dict_utils import merge_nested_dicts, remap_dict
 
 __all__ = [
     "ensure_path",
