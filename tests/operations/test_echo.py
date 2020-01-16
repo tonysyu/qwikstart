b7aca8d9501eef47286672e6e0b24c06ac2fdb0a
Tony S Yu <tsyu80@gmail.com>
2020-01-16 17:35:56 -0600

    Add echo operation

---
+++ tests/operations/test_echo.py
@@ -0,0 +1,35 @@
+from typing import Any, Dict, Optional
+from unittest.mock import Mock, patch
+
+from qwikstart.operations import echo
+
+from .. import helpers
+
+
+class TestEchoOperation:
+    def test_string(self) -> None:
+        mock_print = self.echo("Hello")
+        mock_print.assert_called_once_with("Hello")
+
+    def test_template_string(self) -> None:
+        mock_print = self.echo(
+            "Hello, {{ qwikstart.name }}!", template_variables={"name": "World"}
+        )
+        mock_print.assert_called_once_with("Hello, World!")
+
+    def echo(
+        self, message: str, template_variables: Optional[Dict[str, Any]] = None
+    ) -> Mock:
+        """Return mocked `print` function called by `echo.Operation`."""
+        template_variables = template_variables or {}
+
+        context = echo.Context(
+            execution_context=helpers.get_execution_context(),
+            message=message,
+            template_variables=template_variables,
+        )
+
+        echo_op = echo.Operation()
+        with patch.object(echo, "print") as mock_print:
+            echo_op.run(context)
+        return mock_print
