9c35d47e796b6577a4e9937d320a3cf6b5cc70db
Tony S Yu <tsyu80@gmail.com>
2020-01-19 18:19:49 -0600

    Add choices for user prompts

--- qwikstart/utils/prompt.py
+++ qwikstart/utils/prompt.py
@@ -1,5 +1,6 @@
+import os
 from dataclasses import dataclass
-from typing import Any, Optional
+from typing import Any, List, Optional
 
 import click
 import click.types
@@ -11,6 +12,7 @@ from qwikstart.exceptions import UserFacingError
 class Prompt:
     name: str
     default_value: Optional[Any] = None
+    choices: Optional[List[Any]] = None
 
 
 def create_prompt(**prompt_kwargs: Any) -> Prompt:
@@ -41,8 +43,48 @@ def read_user_variable(prompt: Prompt) -> Any:
 
     Adapted from `cookiecutter.prompt`
     (see https://github.com/cookiecutter/cookiecutter).
+
+    For more info, see https://click.palletsprojects.com/en/7.x/api/#click.prompt
     """
-    # Please see https://click.palletsprojects.com/en/7.x/api/#click.prompt
-    return click.prompt(
-        click.style(prompt.name, fg="green"), default=prompt.default_value
+    if prompt.choices:
+        return read_user_choice(prompt)
+    return click.prompt(default_style(prompt.name), default=prompt.default_value)
+
+
+def read_user_choice(prompt: Prompt) -> Any:
+    """Prompt user to choose from several options for the given variable.
+
+    The first item will be returned if no input provided.
+
+    Adapted from `cookiecutter.prompt`
+    (see https://github.com/cookiecutter/cookiecutter).
+    """
+    choices = prompt.choices
+    if not isinstance(choices, list):
+        msg = f"Choices for prompt must be list but given {type(choices)}: {choices!r}"
+        raise UserFacingError(msg)
+
+    if not choices:
+        raise UserFacingError("Choices for prompt cannot be empty")
+
+    choice_map = {"{}".format(i): value for i, value in enumerate(choices, 1)}
+    numeric_choices = choice_map.keys()
+    default = "1"
+
+    choice_lines = ["{} - {}".format(*c) for c in choice_map.items()]
+    display = os.linesep.join(
+        (
+            "Select {}:".format(prompt.name),
+            os.linesep.join(choice_lines),
+            "Choose from {}".format(", ".join(numeric_choices)),
+        )
     )
+
+    user_choice = click.prompt(
+        display, type=click.Choice(numeric_choices), default=default, show_choices=False
+    )
+    return choice_map[user_choice]
+
+
+def default_style(message: str) -> str:
+    return click.style(message, fg="green")
--- tests/utils/test_prompt.py
+++ tests/utils/test_prompt.py
@@ -1,25 +1,76 @@
+from unittest.mock import Mock, patch
+
 import pytest
 
 from qwikstart.exceptions import UserFacingError
-from qwikstart.utils.prompt import create_prompt
+from qwikstart.utils import prompt as _prompt
 
 
 class TestCreatePrompt:
     def test_name_only(self) -> None:
-        prompt = create_prompt(name="test")
+        prompt = _prompt.create_prompt(name="test")
         assert prompt.name == "test"
         assert prompt.default_value is None
 
     def test_name_and_default(self) -> None:
-        prompt = create_prompt(name="test", default_value="hello")
+        prompt = _prompt.create_prompt(name="test", default_value="hello")
         assert prompt.name == "test"
         assert prompt.default_value == "hello"
 
     def test_name_missing_raises(self) -> None:
         with pytest.raises(UserFacingError, match="Prompt definition has no 'name'"):
-            create_prompt()
+            _prompt.create_prompt()
 
     def test_unknown_attribute_raises(self) -> None:
         msg = "Prompt definition for 'test' has unknown keys:"
         with pytest.raises(UserFacingError, match=msg):
-            create_prompt(name="test", unknown="value")
+            _prompt.create_prompt(name="test", unknown="value")
+
+    @patch.object(_prompt, "Prompt")
+    def test_prompt_raises_unknown_type_error(self, prompt_class: Mock) -> None:
+        prompt_class.__dataclass_fields__ = {"name": str}
+        prompt_class.side_effect = TypeError("Fake error should be re-raised")
+        with pytest.raises(TypeError, match="Fake error should be re-raised"):
+            _prompt.create_prompt(name="test")
+
+
+@patch.object(_prompt, "read_user_choice")
+@patch.object(_prompt.click, "prompt")  # type: ignore
+class TestReadUserVariable:
+    def test_call_click_prompt(self, click_prompt: Mock, read_choice: Mock) -> None:
+        prompt = _prompt.create_prompt(name="test")
+        _prompt.read_user_variable(prompt)
+
+        click_prompt.assert_called_once_with(
+            _prompt.default_style("test"), default=None
+        )
+        read_choice.assert_not_called()
+
+    def test_call_read_choice(self, click_prompt: Mock, read_choice: Mock) -> None:
+        prompt = _prompt.create_prompt(name="greeting", choices=["Hi", "Hello"])
+        _prompt.read_user_variable(prompt)
+
+        read_choice.assert_called_once_with(prompt)
+        click_prompt.assert_not_called()
+
+
+@patch.object(_prompt.click, "prompt")  # type: ignore
+class TestReadUserChoice:
+    def test_select_choice(self, click_prompt: Mock) -> None:
+        prompt = _prompt.create_prompt(name="greeting", choices=["Hi", "Hello"])
+
+        click_prompt.return_value = "1"
+        assert _prompt.read_user_choice(prompt) == "Hi"
+
+        click_prompt.return_value = "2"
+        assert _prompt.read_user_choice(prompt) == "Hello"
+
+    def test_string_choices_raises_type_error(self, click_prompt: Mock) -> None:
+        prompt = _prompt.create_prompt(name="greeting", choices="Hi, Hello")
+        with pytest.raises(UserFacingError, match="Choices for prompt must be list"):
+            _prompt.read_user_choice(prompt)
+
+    def test_empty_choices_raises_value_error(self, click_prompt: Mock) -> None:
+        prompt = _prompt.create_prompt(name="greeting", choices=[])
+        with pytest.raises(UserFacingError, match="Choices for prompt cannot be empty"):
+            _prompt.read_user_choice(prompt)
