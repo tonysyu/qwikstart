2daadfea71f7477c2213380a051659566eac881d
Tony S Yu <tsyu80@gmail.com>
2020-02-29 13:37:51 -0600

    Make shell operation available and allow output storage

--- qwikstart/operations/shell.py
+++ qwikstart/operations/shell.py
@@ -1,30 +1,42 @@
 import logging
 import subprocess
 from dataclasses import dataclass, field
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Optional, Union
 
 from ..base_context import BaseContext
 from ..utils.templates import DEFAULT_TEMPLATE_VARIABLE_PREFIX, TemplateRenderer
 from .base import BaseOperation
+from .utils import TEMPLATE_VARIABLE_PREFIX_HELP
 
 __all__ = ["Operation"]
 
 logger = logging.getLogger(__name__)
 
+CONTEXT_HELP = {
+    "cmd": "Command or list of commands to run.",
+    "template_variable_prefix": TEMPLATE_VARIABLE_PREFIX_HELP,
+}
+
 
 @dataclass(frozen=True)
 class Context(BaseContext):
     cmd: Union[List[str], str]
+    echo_output: bool = True
+    output_var: Optional[str] = None
     template_variables: Dict[str, Any] = field(default_factory=dict)
     template_variable_prefix: str = DEFAULT_TEMPLATE_VARIABLE_PREFIX
 
+    @classmethod
+    def help(cls, field_name: str) -> Optional[str]:
+        return CONTEXT_HELP.get(field_name)
 
-class Operation(BaseOperation[Context, None]):
+
+class Operation(BaseOperation[Context, Dict[str, Any]]):
     """Operation to run an arbitrary shell command."""
 
     name: str = "shell"
 
-    def run(self, context: Context) -> None:
+    def run(self, context: Context) -> Dict[str, Any]:
         renderer = TemplateRenderer.from_context(context)
         if isinstance(context.cmd, list):
             cmd = [renderer.render_string(arg) for arg in context.cmd]
@@ -45,4 +57,7 @@ class Operation(BaseOperation[Context, None]):
         )
         response.check_returncode()
 
-        logger.info(response.stdout)
+        if context.echo_output:
+            logger.info(response.stdout)
+
+        return {context.output_var: response.stdout} if context.output_var else {}
--- tests/operations/test_shell.py
+++ tests/operations/test_shell.py
@@ -14,6 +14,14 @@ class TestShellOperation:
             [call("Running command: echo hello"), call("hello\n")]
         )
 
+    def test_output_not_logged_with_echo_off(self, mock_logger: Mock) -> None:
+        self.shell({"cmd": "echo hello", "echo_output": False})
+        mock_logger.info.assert_called_once_with("Running command: echo hello")
+
+    def test_output_saved_to_variable(self, mock_logger: Mock) -> None:
+        output = self.shell({"cmd": "echo hello", "output_var": "greeting"})
+        assert output == {"greeting": "hello\n"}
+
     def test_list_command(self, mock_logger: Mock) -> None:
         self.shell({"cmd": ["echo", "hello"]})
         mock_logger.info.assert_has_calls(
@@ -31,9 +39,9 @@ class TestShellOperation:
             [call("Running command: echo Howdy"), call("Howdy\n")]
         )
 
-    def shell(self, context_defs: Dict[str, Any]) -> None:
+    def shell(self, context_defs: Dict[str, Any]) -> Dict[str, Any]:
         context = shell.Context(
             execution_context=helpers.get_execution_context(), **context_defs
         )
         shell_op = shell.Operation()
-        shell_op.run(context)
+        return shell_op.run(context)
