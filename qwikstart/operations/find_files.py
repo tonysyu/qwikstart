de9b1b17d2d97cec985ffef48d430b44db832dac
Tony S Yu <tsyu80@gmail.com>
2020-03-04 21:52:14 -0600

    Add path_filter to find_files operation

--- qwikstart/operations/find_files.py
+++ qwikstart/operations/find_files.py
@@ -1,9 +1,11 @@
 import logging
 import os
 import re
+import textwrap
 from dataclasses import dataclass
+from fnmatch import fnmatch
 from pathlib import Path
-from typing import Dict, List, Optional
+from typing import Callable, Dict, List, Optional, cast
 
 from ..base_context import BaseContext
 from .base import BaseOperation
@@ -16,6 +18,14 @@ CONTEXT_HELP = {
     "regex": "Regex to search for in files",
     "directory": "Root directory for search (defaults to working directory).",
     "output_name": "Variable name where list of matching files is stored.",
+    "path_filter": textwrap.dedent(
+        """
+        File filter string passed to `fnmatch` before searching. This can be used
+        to speed up searching for large repositories.
+
+        For example, you can limit text search to json files using "*.json".
+        """
+    ),
 }
 
 
@@ -24,6 +34,7 @@ class Context(BaseContext):
     regex: str
     directory: Optional[Path] = None
     output_name: str = "matching_files"
+    path_filter: Optional[str] = None
 
     @classmethod
     def help(cls, field_name: str) -> Optional[str]:
@@ -38,10 +49,14 @@ class Operation(BaseOperation[Context, Dict[str, List[str]]]):
     def run(self, context: Context) -> Dict[str, List[str]]:
         regex = re.compile(context.regex)
         root_directory = context.directory or "."
+        path_filter = create_path_filter(context.path_filter)
+
         matching_files = []
         for root, dirs, files in os.walk(root_directory):
             for filename in files:
                 filename = os.path.join(root, filename)
+                if not path_filter(filename):
+                    continue
                 try:
                     with open(filename) as f:
                         file_contents = f.read()
@@ -53,3 +68,17 @@ class Operation(BaseOperation[Context, Dict[str, List[str]]]):
                     matching_files.append(filename)
 
         return {context.output_name: matching_files}
+
+
+def create_path_filter(path_filter_string: Optional[str]) -> Callable[[str], bool]:
+    if not path_filter_string:
+
+        def path_filter(path: str) -> bool:
+            return True
+
+    else:
+
+        def path_filter(path: str) -> bool:
+            return fnmatch(path, cast(str, path_filter_string))
+
+    return path_filter
--- tests/operations/test_find_files.py
+++ tests/operations/test_find_files.py
@@ -18,16 +18,16 @@ class TestFindFilesFS(TestCase):
         assert self.find_files("doesn't match anything") == []
 
     def test_single_no_match(self) -> None:
-        self.fs.create_file("/path/to/match.txt")
+        self.fs.create_file("path/to/match.txt")
         assert self.find_files("doesn't match anything") == []
 
     def test_single_file_match(self) -> None:
-        self.fs.create_file("/path/to/match.txt", contents="match")
-        assert self.find_files("match") == ["/path/to/match.txt"]
+        self.fs.create_file("path/to/match.txt", contents="match")
+        assert self.find_files("match") == ["path/to/match.txt"]
 
     def test_multiline_match(self) -> None:
         self.fs.create_file(
-            "/path/to/match.txt",
+            "path/to/match.txt",
             contents=textwrap.dedent(
                 """
                 x
@@ -37,32 +37,37 @@ class TestFindFilesFS(TestCase):
                 """
             ),
         )
-        assert self.find_files("match") == ["/path/to/match.txt"]
+        assert self.find_files("match") == ["path/to/match.txt"]
 
     def test_single_match_in_similarly_named_files(self) -> None:
-        self.fs.create_file("/a/b/file.txt", contents="no-match")
-        self.fs.create_file("/x/y/file.txt", contents="exact-match")
-        assert self.find_files("exact-match") == ["/x/y/file.txt"]
+        self.fs.create_file("a/b/file.txt", contents="no-match")
+        self.fs.create_file("x/y/file.txt", contents="exact-match")
+        assert self.find_files("exact-match") == ["x/y/file.txt"]
+
+    def test_path_filter_skips_file_with_matching_text(self) -> None:
+        self.fs.create_file("file.txt", contents="match")
+        self.fs.create_file("file.md", contents="match")
+        assert self.find_files("match", path_filter="*.md") == ["./file.md"]
 
     def test_multiple_file_matches(self) -> None:
-        self.fs.create_file("/match1.txt", contents="hi")
-        self.fs.create_file("/match2.txt", contents="hello")
-        assert self.find_files("(hi|hello)") == ["/match1.txt", "/match2.txt"]
+        self.fs.create_file("match1.txt", contents="hi")
+        self.fs.create_file("match2.txt", contents="hello")
+        assert self.find_files("(hi|hello)") == ["./match1.txt", "./match2.txt"]
 
     @patch.object(find_files, "logger")
     def test_unreadable_file(self, logger: Mock) -> None:
-        self.fs.create_file("/restricted_file.txt")
-        os.chmod("/restricted_file.txt", 0o000)
+        self.fs.create_file("restricted_file.txt")
+        os.chmod("restricted_file.txt", 0o000)
         assert self.find_files("ignore this") == []
         logger.debug("Failed to read file /restricted_file.txt")
 
     def find_files(
-        self, regex: str, directory: str = "/", output_name: str = "matching_files"
+        self, regex: str, output_name: str = "matching_files", **kwargs: Any
     ) -> Any:
         context = {
             "execution_context": helpers.get_execution_context(),
-            "directory": directory,
             "regex": regex,
+            **kwargs,
         }
         op = find_files.Operation()
         return op.execute(context)[output_name]
